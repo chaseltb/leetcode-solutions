66669ba64dfa4968272a01375700ebb47f093469
Chase "Like the bank <146985322+chaseltb@users.noreply.github.com>
2024-03-27 18:21:32 -0400

    leet_code_151.py time complexity

--- leetcode/medium/leet_code_151.py
+++ leetcode/medium/leet_code_151.py
@@ -1,6 +1,18 @@
-# Reverse words in a string
+# 151 Reverse words in a string
 
-# medium
+# Medium O(n) time
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split()
+        
+        for w in words:
+            w.strip()
+        
+        words = reversed(words)
+
+        reverse_words = ' '.join(words)
+        return reverse_words
 
 '''
 Description:
@@ -12,8 +24,6 @@ A word is defined as a sequence of non-space characters. The words in s will be 
 Return a string of the words in reverse order concatenated by a single space.
 
 Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
-
- 
 
 Example 1:
 
@@ -30,15 +40,3 @@ Input: s = "a good   example"
 Output: "example good a"
 Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
 '''
-
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        words = s.split()
-        
-        for w in words:
-            w.strip()
-        
-        words = reversed(words)
-
-        reverse_words = ' '.join(words)
-        return reverse_words
