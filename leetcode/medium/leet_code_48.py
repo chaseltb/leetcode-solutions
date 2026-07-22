6201a2f69e232e4e2bd221966a664cbc20af2780
Chase Thompson <146985322+chaseltb@users.noreply.github.com>
2026-07-22 17:05:19 -0400

    Update leet_code_48.py

--- leetcode/medium/leet_code_48.py
+++ leetcode/medium/leet_code_48.py
@@ -2,7 +2,6 @@
 
 # Rotate the matrix in place, not allocating another matrix.
 # Medium O(1) Space, O(n^2) Time
-
 import numpy as np
 
 class Solution:
@@ -16,24 +15,3 @@ class Solution:
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
             # Reverse the matrix
             matrix[i].reverse()            
-
-'''
-Description:
-
-You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
-
-You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
-
- 
-
-Example 1:
-
-
-Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
-Output: [[7,4,1],[8,5,2],[9,6,3]]
-Example 2:
-
-
-Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
-Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
-'''
